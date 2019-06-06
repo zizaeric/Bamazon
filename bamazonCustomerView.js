b9aede6a8e7648f4b713ac0a7df46d45ac49a961
zizaeric <zizaeric3@gmail.com>
2019-06-06 08:07:41 -0400

    changed showInventory function to displayAvailableItems, added welcomeToBamazon(), and used cli-table npm package

--- bamazonCustomerView.js
+++ bamazonCustomerView.js
@@ -1,6 +1,7 @@
 // Require npm packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table");
 
 // Create MySQL Connection
 var connection = mysql.createConnection({
@@ -15,38 +16,65 @@ connection.connect(function(err) {
     if (err) {
         console.error("error connected: " + err.stack);
     }
-    console.log("Connection successful!");
-    displayInventory();
+    console.log("~~~~~~~~~~~~~~~ Welcome to Bamazon! ~~~~~~~~~~~~~~~");
+    welcomeToBamazon();
 })
 
+//--------------------------- Bamazon Functions -----------------------------
 
-function displayInventory() {
-	// console.log('___ENTER displayInventory___');
+//----------------- welcomeToBamazon -----------------------------------------
+function welcomeToBamazon() {
+    var welcomeMessagePrompt = [
+        {
+            type: "rawlist",
+            name: "action",
+        message: "\nWhat would you like to do?",
+            choices: [
+                "Buy something",
+                "Exit Bamazon"
+            ]
+        }
+    ]
+    inquirer.prompt(welcomeMessagePrompt).then(function(answer){
+        switch (answer.action) {
+            case "Buy something":
+                console.log("\nHere are the products we have available.");
+                displayAvailableItems();
+                break;
+
+            case "Exit Bamazon":
+                console.log("Come again soon! \nBye Bye!")
+                connection.end();
+                break;
+        }
+
+    });
+}
+//------------------------ displayAvailableItems() --------------------------
+// Displays all items available for sale in a neat table
+
+function displayAvailableItems() {
 
 	// Construct the db query string
 	queryStr = 'SELECT * FROM products';
 
 	// Make the db query
-	connection.query(queryStr, function(err, data) {
-		if (err) throw err;
+	connection.query(queryStr, function(err, res) {
+        if (err) throw err;
+        
+        // CLI table
+        var table = new Table({
+            head: ['Item ID', 'Product Name', 'Price']
+          , colWidths: [10, 40, 15]
+        });
 
-		console.log('Existing Inventory: ');
-		console.log('...................\n');
+        // Populate table with values from the response res
+        for (var i = 0; i < res.length; i++) {
+            table.push(
+                [res[i].item_id, res[i].product_name, "$" + res[i].price]
+            );
+        };
+        console.log(table.toString());
 
-		var strOut = '';
-		for (var i = 0; i < data.length; i++) {
-			strOut = '';
-			strOut += 'Item ID: ' + data[i].item_id + '  //  ';
-			strOut += 'Product Name: ' + data[i].product_name + '  //  ';
-			strOut += 'Department: ' + data[i].department_name + '  //  ';
-			strOut += 'Price: $' + data[i].price + '\n';
-
-			console.log(strOut);
-		}
-
-	  	console.log("---------------------------------------------------------------------\n");
-
-	  	//Prompt the user for item/quantity they would like to purchase
-	  	promptUserPurchase();
 	})
 }
